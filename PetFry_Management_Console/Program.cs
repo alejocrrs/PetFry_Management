3ac3ce6052e7cb57d0111302e1a49d1f09d7b7c6
alejocrrs <alejocrrs@gmail.com>
2022-03-15 07:44:18 -0500

    Menú: Avance 1

--- PetFry_Management_Console/Program.cs
+++ PetFry_Management_Console/Program.cs
@@ -8,8 +8,64 @@ namespace PetFry_Management_Console
 {
     internal class Program
     {
+        static void Menu()
+        {
+            while (true)
+            {
+                Console.WriteLine("=== MENÚ DE OPCIONES ===");
+                Console.WriteLine();
+                Console.WriteLine("[1] Opciones de visualización");
+                Console.WriteLine("[2] Opciones de creación");
+                Console.WriteLine("[3] Opciones de edición");
+                Console.WriteLine("[4] Opciones de eliminación");
+                Console.WriteLine();
+
+                Console.WriteLine("Ingresa el número que corresponda con la opción seleccionada: ");
+                int opcion = Int32.Parse(Console.ReadLine());
+
+                if (opcion == 1)
+                {
+                    Console.WriteLine("=== OPCIONES DE VISUALIZACIÓN ===");
+                    Console.WriteLine("[1] Ver lista de usuarios");
+                    Console.WriteLine("    [1.1] Ver lista de clientes");
+                    Console.WriteLine("    [1.2] Ver lista de prestadores");
+                    Console.WriteLine("[2] Ver lista de mascotas");
+                    Console.WriteLine("[3] Ver lista de artículos");
+                    Console.WriteLine("    [3.1] Ver lista de productos");
+                    Console.WriteLine("    [3.1] Ver lista de servicios");
+                    Console.WriteLine("[4] Ver lista de órdenes");
+                    Console.WriteLine("    [4.1] Ver lista de órdenes de productos");
+                    Console.WriteLine("    [4.1] Ver lista de órdenes de servicios");
+                }
+                else if (opcion == 2)
+                {
+                    Console.WriteLine("=== OPCIONES DE CREACIÓN ===");
+                    Console.WriteLine("[1] Agregar usuario");
+                    Console.WriteLine("    [1.1] Agregar cliente");
+                    Console.WriteLine("    [1.2] Agregar prestador");
+                    Console.WriteLine("[2] Agregar mascota");
+                    Console.WriteLine("[3] Agregar artículo");
+                    Console.WriteLine("    [3.1] Agregar producto");
+                    Console.WriteLine("    [3.1] Agregar servicio");
+                    Console.WriteLine("[4] Agregar orden");
+                    Console.WriteLine("    [4.1] Agregar orden de productos");
+                    Console.WriteLine("    [4.1] Agregar orden de servicios");
+                }
+                else if (opcion == 3)
+                {
+
+                }
+                else if (opcion == 4)
+                {
+
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
+            Menu();
+
             Prestador alejo = new Prestador("1001025610", "1234567a", "Alejandro Córdoba Ríos", "3118372792", "Cr 89 #27-42", "alejocrrs@gmail.com");
 
             Cliente andres = new Cliente("102");
